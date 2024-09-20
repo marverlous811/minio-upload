66dfa69882e55ec76e4a2b5f5dcca6beec24743a
shadowWalker <hieu811@gmail.com>
2024-09-20 16:34:50 +0700

    add to support use s3 signature v4

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,5 @@ if [[ "$YA_RECURSIVE" == "true" ]]; then
   recursive_option="--recursive"
 fi
 
-mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY"
+mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY" --api "$YA_SIGNATURE_VERSION"
 mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
