c97fdfd30cc9fd5431ab2274c078fff1ea0e8fce
shadowWalker <hieu811@gmail.com>
2024-09-20 17:26:21 +0700

    add path style

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,10 @@ if [[ "$YA_RECURSIVE" == "true" ]]; then
   recursive_option="--recursive"
 fi
 
-mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY" --api "$YA_SIGNATURE_VERSION"
+s3_path_style=""
+if [[ "$YA_S3_PATH_STYLE" == "true" ]]; then
+  s3_path_style="--path off"
+fi
+
+mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY" --api "$YA_SIGNATURE_VERSION" ${s3_path_style:+"$s3_path_style"}
 mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
