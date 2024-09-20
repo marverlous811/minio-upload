2b3885d3299455b221107752a3d30238f04296e1
shadowWalker <hieu811@gmail.com>
2024-09-20 17:44:06 +0700

    use extra args instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,5 @@ if [[ "$YA_RECURSIVE" == "true" ]]; then
   recursive_option="--recursive"
 fi
 
-s3_path_style=""
-if [[ "$YA_S3_PATH_STYLE" == "true" ]]; then
-  s3_path_style="--path off"
-fi
-
-mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY" --api "$YA_SIGNATURE_VERSION" ${s3_path_style:+"$s3_path_style"}
+mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY" --api "$YA_SIGNATURE_VERSION" $YA_EXTRA_ARGS
 mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
